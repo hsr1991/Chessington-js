2498802929e40d22ae60d8d2279759802e18d896
hsr1991 <hsr1991@hotmail.co.uk>
2021-01-28 15:10:52 +0000

    pawn can move 2 if its the first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,11 +10,17 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE)  {
-             return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+        if (this.player === Player.WHITE && location.row == 1)  {
+             return [Square.at(location.row + 2, location.col), Square.at(location.row + 1, location.col)]
+        } else if (this.player === Player.WHITE && location.row !== 1) {
+            return [Square.at(location.row + 1, location.col)]
+        } else if (this.player === Player.BLACK && location.row == 6) {
+            return [Square.at(location.row -2, location.col), Square.at(location.row - 1, location.col)]
+        } else if (this.player === Player.BLACK && location.row !== 6)
+            return [Square.at(location.row - 1, location.col)]
+        }
+
         }
       // return [Square.at(1, 0), Square.at(6, 7)]
-    }
-}
+    
+
